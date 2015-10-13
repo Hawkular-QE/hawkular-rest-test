0458386e9e34176b2162dbb6c8c884a3a919bd63
Jeeva Kandasamy <jkandasa@redhat.com>
2015-10-13 15:51:39 +0530

    Fixed all unit test failures

--- src/test/java/org/hawkular/qe/rest/test/inventory/unittest/EnvironmentTest.java
+++ src/test/java/org/hawkular/qe/rest/test/inventory/unittest/EnvironmentTest.java
@@ -34,30 +34,35 @@ import org.testng.annotations.Test;
  */
 public class EnvironmentTest extends InventoryTestBase {
     private static final Logger _logger = LoggerFactory.getLogger(EnvironmentTest.class);
+    private static List<Environment> environments = null;
 
     @Test(dataProvider = "environmentDataProvider", priority = 1)
     public void creatTest(Environment environment) {
         _logger.debug("Creating environment[{}] under tenant[{}]", environment.getId(), environment.getTenantId());
         ClientResponse<String> clientResponse = getHawkularClient().inventory().createEnvironment(environment);
+        _logger.debug("Environment Creation Status:[{}]", clientResponse);
         Assert.assertTrue(clientResponse.isSuccess(), clientResponse.toString());
     }
 
     @SuppressWarnings("unchecked")
     @Test(priority = 2)
     public void listTest() {
-        List<Environment> environments = getHawkularClient().inventory().getEnvironments().getEntity();
-        Assert.assertNotNull(environments);
-        _logger.debug("Number of Environement:[{}], list:[{}]", environments.size(), environments);
-        assertEnvironmentsList(environments, (List<Environment>) getEnvironments());
+        ClientResponse<List<Environment>> clientResponse = getHawkularClient().inventory().getEnvironments();
+        _logger.debug("Get available environment(s) status:[{}]", clientResponse);
+        Assert.assertNotNull(clientResponse.getEntity());
+        _logger.debug("Number of Environement:[{}], list:[{}]", clientResponse.getEntity().size(),
+                clientResponse.getEntity());
+        assertEnvironmentsList(clientResponse.getEntity(), (List<Environment>) getEnvironments());
     }
 
     @Test(dataProvider = "environmentDataProvider", priority = 3)
     public void getTest(Environment environment) {
         _logger.debug("Getting environment[{}] under tenant[{}]", environment.getId(), environment.getTenantId());
-        Environment environmentRx = getHawkularClient().inventory().getEnvironment(environment).getEntity();
-        Assert.assertNotNull(environmentRx);
+        ClientResponse<Environment> clientResponse = getHawkularClient().inventory().getEnvironment(environment);
+        _logger.debug("Get Environment response:[{}]", clientResponse);
+        Assert.assertNotNull(clientResponse.getEntity());
         _logger.debug("Received environment[{}] under tenant[{}]", environment.getId(), environment.getTenantId());
-        assertEnvironments(environmentRx, environment);
+        assertEnvironments(clientResponse.getEntity(), environment);
     }
 
     @Test(dataProvider = "environmentDataProvider", priority = 4)
@@ -75,19 +80,21 @@ public class EnvironmentTest extends InventoryTestBase {
     }
 
     public static List<? extends Object> getEnvironments() {
-        List<Environment> environments = new ArrayList<>();
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("env1" + getRandomId()).get()));
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("_env" + getRandomId()).get()));
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("3env_" + getRandomId()).get()));
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("env-4" + getRandomId()).get()));
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("env-withlooooooooooooooooooooooooooooooooongstring").get()));
-        environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
-                .environment("envwith.dot").get()));
+        if (environments == null) {
+            environments = new ArrayList<>();
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("env1" + getRandomId()).get()));
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("_env" + getRandomId()).get()));
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("3env_" + getRandomId()).get()));
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("env-4" + getRandomId()).get()));
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("env-withlooooooooooooooooooooooooooooooooongstring").get()));
+            environments.add(new Environment(CanonicalPath.of().tenant(TENANT.getId())
+                    .environment("envwith.dot").get()));
+        }
         return environments;
     }
 }
